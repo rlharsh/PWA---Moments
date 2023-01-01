765c32d6cb4868c48815eedc43345806a7a8d213
Ronald Lee Harsh <ronald.harsh@protonmail.com>
2023-01-01 11:22:37 -0600

    Update animations

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -3,10 +3,12 @@ document.getElementById('navi-toggle').addEventListener('change', () => {
     if (document.getElementById('navi-toggle').checked) {
 
         document.getElementById('primary-content-wrapper').classList.add('fade-out');
+        document.getElementById('cta-actions').classList.add('show-cta-actions');
 
     } else {
 
         document.getElementById('primary-content-wrapper').classList.remove('fade-out');
+        document.getElementById('cta-actions').classList.remove('show-cta-actions');
 
     }
 
